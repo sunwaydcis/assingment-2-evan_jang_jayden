fb42fe1ef763f15e85da257a67972f260af710d0
evan-2005 <evan.lojenzhen@gmail.com>
2025-12-01 22:07:52 +0800

    Add parse row function

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -29,4 +29,33 @@ class HotelDataReader {
     profitMargin: BigDecimal
   )
 
+  private def parseRow(row: Map[String, String]): HotelData = {
+    HotelData(
+      row("Booking ID"),
+      row("Date of Booking"),
+      row("Time"),
+      row("Customer ID"),
+      row("Gender"),
+      row("Age").toInt,
+      row("Origin Country"),
+      row("State"),
+      row("Location"),
+      row("Destination Country"),
+      row("Destination City"),
+      row("No. Of People").toInt,
+      row("Check-in date"),
+      row("No of Days").toInt,
+      row("Check-Out Date"),
+      row("Rooms").toInt,
+      row("Hotel Name"),
+      row("Hotel Rating").toDouble,
+      row("Payment Mode"),
+      row("Bank Name"),
+      row("Booking Price[SGD]").toDouble,
+      row("Discount").dropRight(1).toDouble, // Remove "%"
+      row("GST").toDouble,
+      row("Profit Margin").toDouble,
+    )
+  }
+
 }
