2616201e656c81a1d01a18daa0a1b0f5095cd836
evan-2005 <evan.lojenzhen@gmail.com>
2025-12-01 22:07:25 +0800

    Add data case class

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -2,4 +2,31 @@ import com.github.tototoshi.csv.*
 
 class HotelDataReader {
 
+  case class HotelData(
+    bookingId: String,
+    dateOfBooking: String,
+    time: String,
+    customerId: String,
+    gender: String,
+    age: Int,
+    originCountry: String,
+    state: String,
+    location: String,
+    destinationCountry: String,
+    destinationCity: String,
+    numberOfPeople: Int,
+    checkInDate: String,
+    numberOfDays: Int,
+    checkOutDate: String,
+    rooms: Int,
+    hotelName: String,
+    hotelRating: Double,
+    paymentMode: String,
+    bankName: String,
+    bookingPriceSGD: BigDecimal,
+    discount: BigDecimal,
+    gst: BigDecimal,
+    profitMargin: BigDecimal
+  )
+
 }
