c4d857b432d06db486c40843495c79d66cb5949b
evan-2005 <evan.lojenzhen@gmail.com>
2025-12-05 17:09:35 +0800

    Move HotelData outside the HotelDataReader class

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -8,36 +8,38 @@ import com.github.tototoshi.csv.*
 
 // class for reading and parsing data
 
+case class HotelData(
+                      bookingId: String,
+                      dateOfBooking: String,
+                      time: String,
+                      customerId: String,
+                      gender: String,
+                      age: Int,
+                      originCountry: String,
+                      state: String,
+                      location: String,
+                      destinationCountry: String,
+                      destinationCity: String,
+                      numberOfPeople: Int,
+                      checkInDate: String,
+                      numberOfDays: Int,
+                      checkOutDate: String,
+                      rooms: Int,
+                      hotelName: String,
+                      hotelRating: Double,
+                      paymentMode: String,
+                      bankName: String,
+                      bookingPriceSGD: BigDecimal,
+                      discount: BigDecimal,
+                      gst: BigDecimal,
+                      profitMargin: BigDecimal,
+                    )
+
 class HotelDataReader {
 
   // class for data encapsulation
 
-  case class HotelData(
-                        bookingId: String,
-                        dateOfBooking: String,
-                        time: String,
-                        customerId: String,
-                        gender: String,
-                        age: Int,
-                        originCountry: String,
-                        state: String,
-                        location: String,
-                        destinationCountry: String,
-                        destinationCity: String,
-                        numberOfPeople: Int,
-                        checkInDate: String,
-                        numberOfDays: Int,
-                        checkOutDate: String,
-                        rooms: Int,
-                        hotelName: String,
-                        hotelRating: Double,
-                        paymentMode: String,
-                        bankName: String,
-                        bookingPriceSGD: BigDecimal,
-                        discount: BigDecimal,
-                        gst: BigDecimal,
-                        profitMargin: BigDecimal,
-                      )
+
 
   // function for reading csv
   def readData(file: String): List[HotelData] = {
